4a5ef6af1f0be50bf82490f9b27c9014947be771
Jason Quense <monastic.panic@gmail.com>
2018-03-21 12:50:14 -0400

    write in terms of babel-plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,11 +1,16 @@
 import { writeFileSync } from 'fs';
-import { stripIndent } from 'common-tags';
-import { join, dirname, extname, basename } from 'path';
+import { join, dirname, extname, basename, relative } from 'path';
 
-import visitor from './visitor';
+import * as t from '@babel/types';
+import template from '@babel/template';
+import { stripIndent } from 'common-tags';
+
+const buildImport = template('require(FILENAME);');
+
+const STYLES = Symbol('CSSLiteralLoader');
 
 let idx = 0;
-function createFilename({ extension = '.css' }, hostFile) {
+function createFilename(hostFile, { extension = '.css' }) {
   const basepath = join(
     dirname(hostFile),
     basename(hostFile, extname(hostFile)),
@@ -14,18 +19,70 @@ function createFilename({ extension = '.css' }, hostFile) {
 }
 
 export default function plugin() {
-  const styles = [];
-
   return {
-    visitor: {
-      ...visitor(styles, createFilename),
+    pre(file) {
+      if (!file.has(STYLES)) {
+        file.set(STYLES, new Set());
+      }
+    },
 
-      Program: {
-        exit() {
-          styles.forEach(({ path, value }) => {
-            writeFileSync(path, stripIndent([value]));
-          });
-        },
+    post(file) {
+      const { opts } = this;
+
+      let styles = file.get(STYLES);
+      styles = Array.from(styles.values());
+
+      file.metadata['css-literal-loader'] = { styles };
+
+      if (opts.writeFiles) {
+        styles.forEach(({ path, value }) => {
+          writeFileSync(path, stripIndent([value]));
+        });
+      }
+    },
+
+    visitor: {
+      TaggedTemplateExpression(path, { opts, file }) {
+        const styles = file.get(STYLES);
+        const { tagName = 'css' } = opts;
+        const { node } = path;
+
+        if (node.tag.name !== tagName || !path.scope.hasGlobal(tagName)) {
+          return;
+        }
+
+        const parseError = path.buildCodeFrameError(
+          'Could not evaluate css. inline css must be statically analyzable',
+        );
+
+        const { start, end } = node;
+
+        // remove the tag and evaluate as a plain template;
+        path.replaceWith(node.quasi);
+
+        const { confident, value } = path.evaluate();
+
+        if (!confident) {
+          throw parseError;
+        }
+
+        const style = { value, start, end };
+        const getFilename = opts.getFilename || createFilename;
+
+        const hostFile = file.opts.filename;
+        style.path = getFilename(hostFile, opts);
+
+        let filename = relative(dirname(hostFile), style.path);
+
+        if (!filename.startsWith('.')) {
+          filename = `./${filename}`;
+        }
+
+        path.replaceWith(
+          buildImport({ FILENAME: t.StringLiteral(filename) }), // eslint-disable-line new-cap
+        );
+
+        styles.add(style);
       },
     },
   };
--- src/traverse.js
+++ src/traverse.js
@@ -1,65 +1,45 @@
-import { parse } from 'babylon';
-import { codeFrameColumns } from '@babel/code-frame';
-import _traverse, { Hub, NodePath } from '@babel/traverse';
+// import { codeFrameColumns } from '@babel/code-frame';
+import { transformSync } from '@babel/core';
 
-function parseSource(src) {
-  return parse(src, {
-    allowImportExportEverywhere: true,
-    allowReturnOutsideFunction: true,
-    allowSuperOutsideMethod: true,
-    sourceType: 'unambigious',
-    sourceFilename: true,
-    plugins: [
-      'jsx',
-      'flow',
-      'doExpressions',
-      'objectRestSpread',
-      'decorators',
-      'classProperties',
-      'classPrivateProperties',
-      'classPrivateMethods',
-      'exportDefaultFrom',
-      'exportNamespaceFrom',
-      'asyncGenerators',
-      'functionBind',
-      'functionSent',
-      'dynamicImport',
-      'numericSeparator',
-      'optionalChaining',
-      'importMeta',
-      'bigInt',
-      'optionalCatchBinding',
-      'throwExpressions',
-      'pipelineOperator',
-      'nullishCoalescingOperator',
-    ],
-  });
-}
+import plugin from './plugin';
 
-export default function traverse(source, visitors, opts) {
-  const ast = parseSource(source);
-
-  // https://github.com/babel/babel/issues/4640 (closed but reverted)
-  const hub = new Hub({
-    buildCodeFrameError(node, message) {
-      let msg = message;
-      // eslint-disable-next-line no-underscore-dangle
-      const loc = node && (node.loc || node._loc);
-      if (loc) {
-        msg = `${msg} (${loc.start.line}:${loc.start.column})\n`;
-        msg += codeFrameColumns(source, loc);
-      }
-      return new Error(msg);
+export default function traverse(source, filename, opts) {
+  return transformSync(source, {
+    filename,
+    babelrc: false,
+    code: false,
+    ast: false,
+    plugins: [[plugin, opts]],
+    parserOpts: {
+      allowImportExportEverywhere: true,
+      allowReturnOutsideFunction: true,
+      allowSuperOutsideMethod: true,
+      sourceType: 'unambigious',
+      sourceFilename: true,
+      plugins: [
+        'jsx',
+        'flow',
+        'doExpressions',
+        'objectRestSpread',
+        'decorators',
+        'classProperties',
+        'classPrivateProperties',
+        'classPrivateMethods',
+        'exportDefaultFrom',
+        'exportNamespaceFrom',
+        'asyncGenerators',
+        'functionBind',
+        'functionSent',
+        'dynamicImport',
+        'numericSeparator',
+        'optionalChaining',
+        'importMeta',
+        'bigInt',
+        'optionalCatchBinding',
+        'throwExpressions',
+        'pipelineOperator',
+        'nullishCoalescingOperator',
+      ],
     },
   });
-
-  const path = NodePath.get({
-    hub,
-    parentPath: null,
-    parent: ast,
-    container: ast,
-    key: 'program',
-  });
-
-  return _traverse(ast, visitors, path.setContext().scope, { opts });
 }
