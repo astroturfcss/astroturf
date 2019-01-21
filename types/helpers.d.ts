49551dbb6b26788472fd7429ad54523e2a93f754
Jason Quense <monastic.panic@gmail.com>
2019-01-21 16:04:02 -0500

    fix type

--- types/helpers.d.ts
+++ types/helpers.d.ts
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 export type Omit<T, U> = Pick<T, Exclude<keyof T, U>>;
 
+export type mapper<TInner, TOutter> = (input: TInner) => TOutter;
+
 export interface InferableComponentEnhancerWithProps<
   TInjectedProps,
   TNeedsProps
@@ -12,11 +14,11 @@ export interface InferableComponentEnhancerWithProps<
 }
 
 export function mapProps<TInner, TOutter>(
-  propsMapper: (input: TInner) => TOutter,
+  propsMapper: mapper<TOutter, TInner>,
 ): InferableComponentEnhancerWithProps<TInner, TOutter>;
 
 export function withProps<TInner, TOutter>(
-  createProps: TInner | ((input: TInner) => TOutter),
+  createProps: TInner | mapper<TOutter, TInner>,
 ): InferableComponentEnhancerWithProps<TInner & TOutter, TOutter>;
 
 export function defaultProps<TDefaultProps = {}>(
