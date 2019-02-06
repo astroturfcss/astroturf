1ca59bcedb25c76252f7d0dfc51c44c65f440371
Jason Quense <monastic.panic@gmail.com>
2019-02-06 14:59:01 -0500

    feat: trim some more weight off

--- src/runtime/styled.js
+++ src/runtime/styled.js
@@ -1,7 +1,5 @@
-import classNames from 'classnames';
-import React from 'react'; // eslint-disable-line import/no-extraneous-dependencies
+const React = require('react'); // eslint-disable-line import/no-extraneous-dependencies
 
-const has = Object.prototype.hasOwnProperty;
 // eslint-disable-next-line no-control-regex
 const reWords = /[A-Z\xc0-\xd6\xd8-\xde]?[a-z\xdf-\xf6\xf8-\xff]+(?:['’](?:d|ll|m|re|s|t|ve))?(?=[\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000]|[A-Z\xc0-\xd6\xd8-\xde]|$)|(?:[A-Z\xc0-\xd6\xd8-\xde]|[^\ud800-\udfff\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\d+\u2700-\u27bfa-z\xdf-\xf6\xf8-\xffA-Z\xc0-\xd6\xd8-\xde])+(?:['’](?:D|LL|M|RE|S|T|VE))?(?=[\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000]|[A-Z\xc0-\xd6\xd8-\xde](?:[a-z\xdf-\xf6\xf8-\xff]|[^\ud800-\udfff\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\d+\u2700-\u27bfa-z\xdf-\xf6\xf8-\xffA-Z\xc0-\xd6\xd8-\xde])|$)|[A-Z\xc0-\xd6\xd8-\xde]?(?:[a-z\xdf-\xf6\xf8-\xff]|[^\ud800-\udfff\xac\xb1\xd7\xf7\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\xbf\u2000-\u206f \t\x0b\f\xa0\ufeff\n\r\u2028\u2029\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\d+\u2700-\u27bfa-z\xdf-\xf6\xf8-\xffA-Z\xc0-\xd6\xd8-\xde])+(?:['’](?:d|ll|m|re|s|t|ve))?|[A-Z\xc0-\xd6\xd8-\xde]+(?:['’](?:D|LL|M|RE|S|T|VE))?|\d*(?:1ST|2ND|3RD|(?![123])\dTH)(?=\b|[a-z_])|\d*(?:1st|2nd|3rd|(?![123])\dth)(?=\b|[A-Z_])|\d+|(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff]|\ud83c[\udffb-\udfff])?)*/g;
 
@@ -11,18 +9,9 @@ const camelCase = str =>
     '',
   );
 
-export function styled(
-  type,
-  options,
-  displayName,
-  styles,
-  kebabName,
-  camelName,
-) {
+function styled(type, options, displayName, styles, kebabName, camelName) {
   options = options || { allowAs: typeof type === 'string' };
-  const componentClassName = has.call(styles, kebabName)
-    ? styles[kebabName]
-    : styles[camelName];
+  const componentClassName = styles[kebabName] || styles[camelName];
 
   // always passthrough if the type is a styled component
   const allowAs = type.isAstroturf ? false : options.allowAs;
@@ -35,45 +24,41 @@ export function styled(
     const childProps = { ...props, ref };
     if (allowAs) delete childProps.as;
 
-    const modifierClassNames = [];
+    const classNames = [childProps.className || '', componentClassName];
 
     if (hasModifiers) {
       Object.keys(props).forEach(propName => {
         const propValue = props[propName];
+        const typeOf = typeof propValue;
 
-        if (typeof propValue === 'boolean' || propValue == null) {
-          if (has.call(styles, propName)) {
+        if (typeOf === 'boolean' || propValue == null) {
+          if (styles[propName]) {
             if (propValue) {
-              modifierClassNames.push(styles[propName]);
+              classNames.push(styles[propName]);
             }
 
             delete childProps[propName];
           } else {
             const camelPropName = camelCase(propName);
 
-            if (has.call(styles, camelPropName)) {
+            if (styles[camelPropName]) {
               if (propValue) {
-                modifierClassNames.push(styles[camelPropName]);
+                classNames.push(styles[camelPropName]);
               }
-
               delete childProps[propName];
             }
           }
-        } else if (
-          typeof propValue === 'string' ||
-          typeof propValue === 'number'
-        ) {
+        } else if (typeOf === 'string' || typeOf === 'number') {
           const propKey = `${propName}-${propValue}`;
-          if (has.call(styles, propKey)) {
-            modifierClassNames.push(styles[propKey]);
 
+          if (styles[propKey]) {
+            classNames.push(styles[propKey]);
             delete childProps[propName];
           } else {
             const camelPropKey = camelCase(propKey);
 
-            if (has.call(styles, camelPropKey)) {
-              modifierClassNames.push(styles[camelPropKey]);
-
+            if (styles[camelPropKey]) {
+              classNames.push(styles[camelPropKey]);
               delete childProps[propName];
             }
           }
@@ -81,11 +66,7 @@ export function styled(
       });
     }
 
-    childProps.className = classNames(
-      childProps.className,
-      componentClassName,
-      ...modifierClassNames,
-    );
+    childProps.className = classNames.join(' ');
 
     return React.createElement(
       allowAs && props.as ? props.as : type,
@@ -107,8 +88,11 @@ export function styled(
   return decorated;
 }
 
-export default styled;
+module.exports = styled;
+module.exports.styled = styled;
 
-export const css = () => {
-  throw new Error('`css` template literal evaluated at runtime!');
-};
+if (__DEV__) {
+  module.exports.css = () => {
+    throw new Error('`css` template literal evaluated at runtime!');
+  };
+}
--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,6 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// eslint-disable-next-line no-underscore-dangle
+global.__DEV__ = true;
