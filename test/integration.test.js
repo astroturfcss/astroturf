202123c2f9e11a555444797be4f39d0ed6e21a01
Jason Quense <monastic.panic@gmail.com>
2019-10-10 14:02:47 -0400

    feat: only warn on possible cycle

--- test/integration.test.js
+++ test/integration.test.js
@@ -99,7 +99,7 @@ describe('webpack integration', () => {
     expect(message).toEqual(expect.stringContaining('Imported as MyButton'));
   });
 
-  it('should throw on cycles', async () => {
+  xit('should throw on cycles', async () => {
     const timeout = global.setTimeout;
 
     jest.spyOn(global, 'setTimeout').mockImplementation(fn => timeout(fn, 0));
