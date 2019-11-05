afdb37177a7b496faebf8deee7726aa31fba40d3
Jason Quense <monastic.panic@gmail.com>
2019-11-05 17:17:31 -0500

    fix: composes on wrong class (#471)
    fixes #365

--- src/utils/wrapInClass.js
+++ src/utils/wrapInClass.js
@@ -2,39 +2,30 @@ import { stripIndents } from 'common-tags';
 
 export default function wrapInClass(text) {
   const imports = [];
-  const composes = [];
 
   let match;
   const rImports = /@import.*?(?:$|;)/g;
-  const rComposes = /\b(?:composes\s*?:\s*(.*?)(?:from\s(.+?))?(?=[}\n\r]))/gim;
 
   // eslint-disable-next-line no-cond-assign
   while ((match = rImports.exec(text))) {
     imports.push(match[0]);
   }
-  match = null;
-  // eslint-disable-next-line no-cond-assign
-  while ((match = rComposes.exec(text))) {
-    composes.push(match[0]);
-  }
 
-  text = text.replace(rImports, '').replace(rComposes, '');
+  text = text.replace(rImports, '');
 
-  // comment prevents Sass from removing the empty class
-  let val = stripIndents`
-    .cls1 {
-      ${text.trim() || '/*!*/'}
-    }`;
-
-  // Components need two css classes, the first being the single root
-  // class with styles and the second is the exported name used in the Styled helper
+  // Components need two css classes, the actual style declarations and a hook class.
   // We need both so that that interpolations have a class that is _only_
   // the single class, e.g. no additional classes composed in so that it can be used
-  // as a selecto
-  if (composes.length) {
-    composes.unshift(`composes: cls1;`);
-    val += `\n.cls2 {\n${composes.join('\n')}\n}`;
-  }
+  // as a selector
+  //
+  // comment prevents Sass from removing the empty class
+  let val = stripIndents`
+    .cls1 { /*!*/ }
+    .cls2 {
+      composes: cls1;
+
+      ${text.trim()}
+    }`;
 
   if (imports.length) val = `${imports.join('\n')}\n${val}`;
   return val;
